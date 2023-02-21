29a4eec98da05f3c7b58deaa951629c2429c933f
Riley Lass <riley@storycord.com>
2023-02-21 15:43:42 -0600

    Created Prop Blueprint

--- Source/WyweeGunGame/Prop.cpp
+++ Source/WyweeGunGame/Prop.cpp
@@ -9,6 +9,9 @@ AProp::AProp()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	RootComponent = MeshComp;
+
 }
 
 // Called when the game starts or when spawned
